c358a3e6cd1e676f85fc75e4099b210d2d16e88a
Mohammed Zaid <zaidzaid0342@gmail.com>
2025-10-25 20:33:16 -0700

    update

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['https://hospitalfron.netlify.app'], // replace with your actual frontend URL
+  origin: ['https://hospital-4tj4.vercel.app/'], // replace with your actual frontend URL
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
